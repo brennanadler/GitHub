89a523403e098444f0e9e6bd3a8f1c488b46e0c5
brennanadler <bballers13@gmail.com>
2015-01-20 13:24:14 -0500

    Update StartScreen.swift

--- StartScreen.swift
+++ StartScreen.swift
@@ -1,1 +1,29 @@
-StartScreen.swift
+//
+//  GameScene.swift
+//  GitHub
+//
+//  Created by Brennan Adler on 1/20/15.
+//  Copyright (c) 2015 Brennan Adler and Sean Rayment. All rights reserved.
+//
+
+import SpriteKit
+
+class StartScreen: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+    
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        
+        for touch: AnyObject in touches {
+            let location = touch.locationInNode(self)
+            
+        }
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}

e40ca30b721133d91e49b01ee73331e5400538b0
brennanadler <bballers13@gmail.com>
2015-04-01 10:55:48 -0400

    Bat Animation Added

--- Enemy.swift
+++ Enemy.swift
@@ -7,7 +7,7 @@ class Enemy: SKSpriteNode {
     class func createEnemy(location: CGPoint) -> Enemy {
         let heroAtlas = SKTextureAtlas(named: "wizard.atlas")
         
-        let sprite = Enemy(texture: heroAtlas.textureNamed("fireball1"))
+        let sprite = Enemy(texture: heroAtlas.textureNamed("10XBat1"))
         sprite.position = location
         let spriteSize = CGSizeMake(sprite.size.width, sprite.size.height)
         let spriteCenter = CGPointMake(sprite.position.x/2, sprite.position.y/2)
@@ -17,10 +17,20 @@ class Enemy: SKSpriteNode {
             physics.affectedByGravity = false
             physics.allowsRotation = false
             physics.velocity = CGVectorMake(-300, 0)
+            physics.mass = 0
         }
         
-        sprite.xScale = 5.0
-        sprite.yScale = 5.0
+        let bat_anim = SKAction.animateWithTextures([
+            heroAtlas.textureNamed("10XBat1"),
+            heroAtlas.textureNamed("10XBat2")
+            ], timePerFrame: 0.12)
+        
+        let run = SKAction.repeatActionForever(bat_anim)
+        
+        sprite.runAction(run, withKey: "bat")
+        
+        sprite.xScale = 0.4
+        sprite.yScale = 0.4
         
         return sprite
     }

b7a46c6dc58846d0e1fe6bf705ab409b9c485b9f
seanrayment <raymentse@belmonthill.org>
2015-04-03 17:29:01 -0400

    Update Enemy.swift

--- Enemy.swift
+++ Enemy.swift
@@ -9,7 +9,7 @@ class Enemy: SKSpriteNode {
         
         let sprite = Enemy(texture: heroAtlas.textureNamed("10XBat1"))
         sprite.position = location
-        let spriteSize = CGSizeMake(sprite.size.width, sprite.size.height)
+        let spriteSize = CGSizeMake(sprite.size.width/3, sprite.size.height/3)
         let spriteCenter = CGPointMake(sprite.position.x/2, sprite.position.y/2)
         
         sprite.physicsBody = SKPhysicsBody(rectangleOfSize: spriteSize, center: spriteCenter)
@@ -18,6 +18,11 @@ class Enemy: SKSpriteNode {
             physics.allowsRotation = false
             physics.velocity = CGVectorMake(-300, 0)
             physics.mass = 0
+            physics.restitution = 0
+            physics.friction = 0
+            physics.linearDamping = 0
+            //physics.usesPreciseCollisionDetection = false
+            //physics.collisionBitMask = 0
         }
         
         let bat_anim = SKAction.animateWithTextures([

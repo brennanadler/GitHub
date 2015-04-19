8c7fd062c3bacd1b14373b342564c9ad0c972660
brennanadler <bballers13@gmail.com>
2015-04-19 00:46:04 -0400

    Create OptionMenu.swift

---
+++ OptionMenu.swift
@@ -0,0 +1,21 @@
+//
+//  OptionMenu.swift
+//  GitHub
+//
+//  Created by Brennan Adler on 4/18/15.
+//  Copyright (c) 2015 Brennan Adler. All rights reserved.
+//
+
+import SpriteKit
+
+
+class OptionMenu: SKScene{
+    
+    override func didMoveToView(view: SKView)
+    {
+        
+    }
+    
+    
+    
+}

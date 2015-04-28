c5a305c675f1beda928910713b08494b3f3b600a
brennanadler <bballers13@gmail.com>
2015-04-28 19:21:01 -0400

    Update MainMenu.swift

--- MainMenu.swift
+++ MainMenu.swift
@@ -29,8 +29,9 @@ class MainMenu: SKScene
 {
     let heroAtlas = SKTextureAtlas(named: "wizard.atlas")
     var Screen: SKNode!
-    var Game = false
-    var ScoreBoard: UITextField!
+    var ScoreBoarder: UITextField!
+    var HighScoreBoard: UITextField!
+    var PreviousScore: Int!
     
     
     override func didMoveToView(view: SKView)
@@ -41,19 +42,20 @@ class MainMenu: SKScene
         addStartButton()
         addOptionButton()
         addStoreButton()
-        //NSNotificationCenter.defaultCenter().postNotificationName("showadsID", object: nil)
+        addHighScore()
+        addScoreBoard()
     }
-
-
+    
+    
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent)
     {
-    
+        
         for touch: AnyObject in touches
         {
             
             let location = touch.locationInNode(self)
             var sprites = nodesAtPoint(location)
-        
+            
             for sprite in sprites
             {
                 if let spriteNode = sprite as? SKSpriteNode
@@ -70,16 +72,17 @@ class MainMenu: SKScene
                         
                         if spriteNode.name == "StartButton"
                         {
-        
+                            
                             if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene
                             {
                                 skView.presentScene(scene)
                                 /* Set the scale mode to scale to fit the window */
                                 scene.scaleMode = .AspectFill
                                 
-                                if(Game){
-                                    ScoreBoard.removeFromSuperview()
-                                }
+                                ScoreBoarder.removeFromSuperview()
+                                HighScoreBoard.removeFromSuperview()
+                            
+                            
                             }
                             
                         }else if(spriteNode.name == "OptionButton"){
@@ -141,22 +144,36 @@ class MainMenu: SKScene
         ShopButton.yScale = 0.0015
         addChild(ShopButton)
     }
-    
-    func addScoreBoard(PScore: Int){
-    ScoreBoard = UITextField(frame: CGRect(x: 520, y: 26, width: 300, height: 20))
-    ScoreBoard.backgroundColor = UIColor(red: 70/255, green: 120/255, blue: 180/255, alpha: 1.0)
-    ScoreBoard.text = "Previous: \(PScore)"
-    ScoreBoard.textColor = UIColor.blackColor()
-    self.view?.addSubview(ScoreBoard)
-    
-        var preHScore: Int = NSUserDefaults.standardUserDefaults().integerForKey("HighScore")
+    func addHighScore(){
         
+        HighScoreBoard = UITextField(frame: CGRect(x: 16, y: 26, width: 300, height: 20))
+        HighScoreBoard.backgroundColor = UIColor(red: 70/255, green: 120/255, blue: 180/255, alpha: 0.0)
+        var HScore: Int = NSUserDefaults.standardUserDefaults().integerForKey("HighScore")
+        HighScoreBoard.text = "High Score: \(HScore)"
+        HighScoreBoard.textColor = UIColor.blackColor()
+        self.view?.addSubview(HighScoreBoard)
+        
+    }
+    
+    func addScoreBoard(){
+        ScoreBoarder = UITextField(frame: CGRect(x: 520, y: 26, width: 300, height: 20))
+        ScoreBoarder.backgroundColor = UIColor(red: 70/255, green: 120/255, blue: 180/255, alpha: 0.0)
+        if(PreviousScore != nil){
+            ScoreBoarder.text = "Previous: \(PreviousScore)"
+        }
+        ScoreBoarder.textColor = UIColor.blackColor()
+        self.view?.addSubview(ScoreBoarder)
+    }
+    
+    func updateHScore(PScore:Int){
+        PreviousScore = PScore
+        var preHScore: Int = NSUserDefaults.standardUserDefaults().integerForKey("HighScore")
         if(PScore >  preHScore){
             NSUserDefaults.standardUserDefaults().setObject(PScore, forKey: "HighScore")
             println(PScore)
+            
         }
-    
     }
-
-
+    
+    
 }

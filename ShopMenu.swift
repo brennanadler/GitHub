deab4a2890c2f019ab9b00aa77d937a27e451fde
brennanadler <bballers13@gmail.com>
2015-04-29 14:14:32 -0400

    Update ShopMenu.swift

--- ShopMenu.swift
+++ ShopMenu.swift
@@ -6,18 +6,25 @@
 //  Copyright (c) 2015 Brennan Adler. All rights reserved.
 //
 
-import Foundation
 import SpriteKit
 
 
 class ShopMenu: SKScene
 {
-    
+    var Screen: SKSpriteNode!
     
     override func didMoveToView(view: SKView)
     {
-
-
+        Screen = SKSpriteNode()
+        self.addChild(Screen)
+        addBg()
+    }
+    
+    func addBg(){
+        let MainMenu = SKSpriteNode(imageNamed: "MainMenu")
+        MainMenu.position = CGPointMake(frame.width / 2, frame.height / 2)
+        addChild(MainMenu)
+        
     }
     
     

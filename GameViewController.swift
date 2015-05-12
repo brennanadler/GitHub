2372270cdc6464c1dc73b1be4d49b71a29cb81b9
brennanadler <bballers13@gmail.com>
2015-05-12 14:33:19 -0400

    Update GameViewController.swift

--- GameViewController.swift
+++ GameViewController.swift
@@ -1,12 +1,28 @@
+
+
+
+
+
+
+
+
+
 import UIKit
 import SpriteKit
 import iAd
 
 
-class GameViewController: UIViewController{
+class GameViewController: UIViewController,ADInterstitialAdDelegate {
+    
+    var interstitial:ADInterstitialAd!
+    var placeHolderView:UIView!
+    var closeButton:UIButton!
+    var skView:SKView!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         //finds dimensions of screen (ipad vs iphone etc.)
         var bounds: CGRect = UIScreen.mainScreen().bounds
@@ -16,21 +32,22 @@ class GameViewController: UIViewController{
         //Makes a global variable to store the Scale variables (Default is iPhone6)
         NSUserDefaults.standardUserDefaults().setFloat(Float(width / 667), forKey: "xScale")
         NSUserDefaults.standardUserDefaults().setFloat(Float(height / 375), forKey: "yScale")
-        println("width: \(width), height: \(height)")
-        println("xScaler, yScaler \(width/667) ,\(height/375)")
         
         //creates permanent Integer named "HighScore" that is an integer... Default value 0
         NSUserDefaults.standardUserDefaults().setInteger(0, forKey: "HighScore")
-        NSUserDefaults.standardUserDefaults().setInteger(50, forKey: "Coins")
+        NSUserDefaults.standardUserDefaults().setInteger(50, forKey: "Gems")
         
+        //Skin Value
+        NSUserDefaults.standardUserDefaults().setObject("_1", forKey: "SkinSuffix")
         
-        //Notification that will run the func hideBannerAd() and has name of hideadsID
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "hideBannerAd", name: "hideadsID", object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "showBannerAd", name: "showadsID", object: nil)
+        //Notification that will run the func runAdd() and has name of runadsID
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: ("runAd:"),    name: "runadsID", object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: ("loadAd:"),    name: "loadadsID", object: nil)
+
         
         let scene = MainMenu()
         // Configure the view.
-        let skView = self.view as SKView
+        skView = self.view as SKView
         skView.showsFPS = true
         skView.showsNodeCount = true
         
@@ -65,4 +82,95 @@ class GameViewController: UIViewController{
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
+    
+    //deletes previous ad and creates a new one, triggers at start of game to give it time to load
+    func loadAd(notification:NSNotification){
+        cycleInterstitial()
+    }
+    
+    //iAD interstitial
+    func runAd(notification:NSNotification){
+        //var timer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: Selector("dislayiAdInterstitial"), userInfo: nil, repeats: false)
+        dislayiAdInterstitial()
+    }
+    
+    func cycleInterstitial(){
+        
+        // Clean up the old interstitial...
+        //  interstitial.delegate = nil;
+        // and create a new interstitial. We set the delegate so that we can be notified of when
+        interstitial = ADInterstitialAd()
+        interstitial.delegate = self;
+
+    }
+    
+    func presentInterlude(){
+        // If the interstitial managed to load, then we'll present it now.
+        println("is ad loaded?")
+        if (interstitial.loaded) {
+            placeHolderView = UIView(frame: self.view.frame)
+            self.view.addSubview(placeHolderView)
+            
+            interstitial.presentInView(placeHolderView)
+            
+            var timer = NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: Selector("addCloser"), userInfo: nil, repeats: false)
+            
+        }
+    }
+    
+    func addCloser(){
+        closeButton = UIButton(frame: CGRect(x: skView.bounds.width / 30
+            , y:  skView.bounds.height / 20, width: 25, height: 25))
+        closeButton.setBackgroundImage(UIImage(named: "Exit"), forState: UIControlState.Normal)
+        closeButton.addTarget(self, action: Selector("close"), forControlEvents: UIControlEvents.TouchDown)
+        self.view.addSubview(closeButton)
+    }
+    // iAd Delegate Mehtods
+    
+    // When this method is invoked, the application should remove the view from the screen and tear it down.
+    // The content will be unloaded shortly after this method is called and no new content will be loaded in that view.
+    // This may occur either when the user dismisses the interstitial view via the dismiss button or
+    // if the content in the view has expired.
+    
+    func interstitialAdDidUnload(interstitialAd: ADInterstitialAd!){
+        self.placeHolderView.removeFromSuperview()
+        
+        closeButton.removeFromSuperview()
+        interstitial = nil
+        
+        cycleInterstitial()
+    }
+    
+    
+    func interstitialAdActionDidFinish(_interstitialAd: ADInterstitialAd!){
+        placeHolderView.removeFromSuperview()
+        closeButton.removeFromSuperview()
+        interstitial = nil
+        
+        println("called just before dismissing - action finished")
+        
+    }
+    
+    // This method will be invoked when an error has occurred attempting to get advertisement content.
+    // The ADError enum lists the possible error codes.
+    func interstitialAd(interstitialAd: ADInterstitialAd!,
+        didFailWithError error: NSError!){
+            cycleInterstitial()
+    }
+    
+    
+    //Load iAd interstitial
+    func dislayiAdInterstitial() {
+        //iAd interstitial
+        presentInterlude()
+    }
+    
+    
+    func close() {
+        placeHolderView.removeFromSuperview()
+        closeButton.removeFromSuperview()
+        interstitial = nil
+        
+    }
+    
 }

4c5ed1a14cb1f8a2d97f093943044a947765f0a5
brennanadler <bballers13@gmail.com>
2015-05-21 19:52:18 -0400

    Update OptionMenu.swift

--- OptionMenu.swift
+++ OptionMenu.swift
@@ -42,7 +42,7 @@ class OptionMenu: SKScene
         
     }
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent)
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent)
     {
         
         
